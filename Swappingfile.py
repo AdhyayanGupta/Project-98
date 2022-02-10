0c483a06b5a63dce69bbd0c0f97b1622888d419a
Adhyayan Gupta <74090647+AdhyayanGupta@users.noreply.github.com>
2022-02-10 17:29:44 +0530

    Add files via upload

---
+++ Swappingfile.py
@@ -0,0 +1,15 @@
+file1 = input("ENTER YOUR FILE DESTINATION")
+file2 = input("ENTER YOUR FILE DESTINATION")
+
+with open(file1,'r') as a:
+    data_a = a.read()
+
+with open(file2,'r') as a:
+    data_b = a.read()
+
+with open(file1,'w') as a:
+    a.write(data_b)
+
+with open(file2,'w') as a:
+    a.write(data_a)
+
